8437cd7ea4a58686ccbd43fe369d95148957807c
Krut Patel <kroot.patel@gmail.com>
2019-06-27 20:31:54 +0530

    scripts: Handle paths with spaces in win install
    Fixes #12

--- scripts/windows-install.bat
+++ scripts/windows-install.bat
@@ -3,28 +3,28 @@ for /f "delims=\" %%a in ("%cd%") do if "%%~nxa"=="scripts" cd ..
 
 where pipenv >NUL 2>&1 || echo Please install pipenv first. && goto :EOF
 
-set install-dir=%LOCALAPPDATA%\trakt-scrobbler\
+set install-dir="%LOCALAPPDATA%\trakt scrobbler"
 echo Installing to %install-dir%
 
-xcopy /s /r /y .\trakt_scrobbler %install-dir% >NUL
-xcopy /r /y Pipfile %install-dir% >NUL
-xcopy /r /y Pipfile.lock %install-dir% >NUL
+xcopy /i /s /r /y .\trakt_scrobbler %install-dir%\ >NUL
+xcopy /i /r /y Pipfile %install-dir%\ >NUL
+xcopy /i /r /y Pipfile.lock %install-dir%\ >NUL
 
 pushd %install-dir%
 pipenv install
 
 for /F "tokens=* USEBACKQ" %%F in (`pipenv --venv`) do set venv_path=%%F
-set py_path=%venv_path%\Scripts\pythonw.exe
+set py_path="%venv_path%\Scripts\pythonw.exe"
 
 echo Adding to startup commands.
 set batch_path="%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\trakt-scrobbler.bat"
 echo @echo off >%batch_path%
 echo pushd %install-dir% >>%batch_path%
-echo start %py_path% %install-dir%main.py >>%batch_path%
+echo start %py_path% %install-dir%\main.py >>%batch_path%
 
 echo Setup complete. Starting device authentication.
 pipenv run python -c "import trakt_interface; trakt_interface.get_access_token()"
 popd
-%py_path% %install-dir%main.py
+%py_path% %install-dir%\main.py
 echo Done.
 :EOF
