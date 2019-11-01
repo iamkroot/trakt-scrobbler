5cb71d0dab83b0249d0d7a772a6a8f671f2f33d2
Krut Patel <kroot.patel@gmail.com>
2019-11-01 18:39:59 +0530

    app_dirs: Fix invalid directories in Windows

--- trakt_scrobbler/app_dirs.py
+++ trakt_scrobbler/app_dirs.py
@@ -2,7 +2,7 @@ import appdirs
 from pathlib import Path
 
 NAME = "trakt-scrobbler"
-DATA_DIR = Path(appdirs.user_data_dir(NAME, roaming=True))
-CFG_DIR = Path(appdirs.user_config_dir(NAME, roaming=True))
+DATA_DIR = Path(appdirs.user_data_dir(NAME, appauthor=False, roaming=True))
+CFG_DIR = Path(appdirs.user_config_dir(NAME, appauthor=False, roaming=True))
 DATA_DIR.mkdir(exist_ok=True, parents=True)
 CFG_DIR.mkdir(exist_ok=True, parents=True)
