b480623bad46373539f1cf349b9d7e87b003ec4f
iamkroot <kroot.patel@gmail.com>
2020-12-19 23:36:06 +0530

    backlog: Add UnknownItems class
    If we are unable to find the correct trakt id for a movie or a show
    when trying to clear the backlog, it will fail to be added to history.
    Earlier, it was just kept around for 30 days, and then deleted. This
    commit introduces the class 'UnknownItems' to keep track of such media.
    Now, invalid media will be removed from backlog, and added to this class
    so that user may manually specify what to do with each item. For example
    they may choose to simply delete the item, and with it, our knowledge of
    the watch timestamp. Or, they may specify a Trakt ID for the item, so as
    to not lose the played history for that media.

--- trakt_scrobbler/backlog_cleaner.py
+++ trakt_scrobbler/backlog_cleaner.py
@@ -1,5 +1,4 @@
 import confuse
-import time
 from collections import defaultdict
 from copy import deepcopy
 from threading import Timer
@@ -9,49 +8,52 @@ from trakt_scrobbler.utils import read_json, write_json
 from trakt_scrobbler import trakt_interface as trakt
 
 
+def _read_shows(shows: dict):
+    for show in shows.values():
+        seasons = defaultdict(dict)
+        for season, episodes in show["seasons"].items():
+            # JSON keys have to be strings. Convert the nums to int manually.
+            seasons[int(season)] = {int(num): v for num, v in episodes.items()}
+        show["seasons"] = seasons
+
+
 class BacklogCleaner:
     BACKLOG_PATH = DATA_DIR / "watched_backlog.json"
     DEFAULT_BACKLOG = {"movies": {}, "shows": {}}
 
     def __init__(self, manual=False):
         self.clear_interval = config["backlog"]["clear_interval"].get(confuse.Number())
-        self.expiry = config["backlog"]["expiry"].get(confuse.Number())
-        self.read_backlog()
+        self.unknown_items = UnknownItems()
         self.timer_enabled = not manual
         if self.timer_enabled:
             self._make_timer()
             self.clear()
 
+    @property
+    def backlog(self):
+        return self.read_backlog()
+
+    @backlog.setter
+    def backlog(self, value):
+        self._backlog = value
+        self.save_backlog()
+
     def read_backlog(self):
         backlog = read_json(self.BACKLOG_PATH)
         if not backlog:
-            self.backlog = deepcopy(self.DEFAULT_BACKLOG)
+            self._backlog = deepcopy(self.DEFAULT_BACKLOG)
         elif isinstance(backlog, dict):
-            for show in backlog["shows"].values():
-                seasons = defaultdict(dict)
-                for season, episodes in show["seasons"].items():
-                    # JSON keys have to be strings. Convert the nums to int manually.
-                    seasons[int(season)] = {int(num): v for num, v in episodes.items()}
-                show["seasons"] = seasons
-            self.backlog = backlog
+            _read_shows(backlog["shows"])
+            self._backlog = backlog
         elif isinstance(backlog, list):  # old backlog style
             # import the items into new style
-            self.backlog = deepcopy(self.DEFAULT_BACKLOG)
+            self._backlog = deepcopy(self.DEFAULT_BACKLOG)
             for item in backlog:
                 self.add(item)
+        return self._backlog
 
     def save_backlog(self):
-        write_json(self.backlog, self.BACKLOG_PATH)
-
-    def remove_expired(self):
-        not_expired = []
-        for item in self.backlog:
-            if item["updated_at"] + self.expiry > time.time():
-                not_expired.append(item)
-            else:
-                logger.warning(f"Item expired: {item}")
-        self.backlog = not_expired
-        self.save_backlog()
+        write_json(self._backlog, self.BACKLOG_PATH)
 
     def _make_timer(self):
         self.timer = Timer(self.clear_interval, self.clear)
@@ -83,17 +85,70 @@ class BacklogCleaner:
 
         self.save_backlog()
 
+    def mark_invalid(self, category, key):
+        item = self.backlog[category].pop(key)
+        self.unknown_items.add(category, key, item)
+
     def clear(self):
-        self.remove_expired()
-        trakt.bulk_add_to_history(self.backlog)
-        self.save_backlog()
+        result = trakt.bulk_add_to_history(self.backlog)
+        added, invalid = {}, {}
+        if result is not False:
+            invalid, resp = result
+            for category, keys in invalid.items():
+                for key in keys:
+                    self.mark_invalid(category, key)
+            added = resp.get("added", {})
+            if any(added.values()):
+                logger.info(f"Added to history: {added}")
+            if any(resp.get("not_found", {}).values()):
+                logger.warning(f"Not found on trakt: {resp['not_found']}")
+            self.backlog = deepcopy(self.DEFAULT_BACKLOG)
 
         if self.timer_enabled:
             self.timer.cancel()
             self._make_timer()
 
+        return result is not False, added, invalid
+
     def purge(self):
         old_backlog = self.backlog
         self.backlog = deepcopy(self.DEFAULT_BACKLOG)
-        self.save_backlog()
         return old_backlog
+
+
+class UnknownItems:
+    """Represents the watched movies or shows for which we couldn't find any TraktID"""
+    UNKNOWN_ITEMS_PATH = DATA_DIR / "unknown_items.json"
+
+    @property
+    def unknown_items(self):
+        return self.read()
+
+    @unknown_items.setter
+    def unknown_items(self, value):
+        self._unknown_items = value
+        self.save()
+
+    def read(self):
+        self._unknown_items = read_json(self.UNKNOWN_ITEMS_PATH) or \
+            deepcopy(BacklogCleaner.DEFAULT_BACKLOG)
+        _read_shows(self._unknown_items["shows"])
+        return self._unknown_items
+
+    def save(self):
+        write_json(self._unknown_items, self.UNKNOWN_ITEMS_PATH)
+
+    def add(self, category, key, item):
+        logger.info(f"Adding to unknown {category}: {item}")
+        if category == "movies":
+            self.unknown_items["movies"][key] = item
+        elif category == "shows":
+            show = self.unknown_items["shows"].setdefault(key, {
+                "media_info": item["media_info"],
+                "seasons": defaultdict(dict)
+            })
+            for season, episodes in item["seasons"].items():
+                show["seasons"][season].update(episodes)
+        self.save()
+
+    # TODO: Allow the user to manually specify correct TraktIDs
