cff65bdfddf912395ce79d01220dc849f9b5f05d
iamkroot <kroot.patel@gmail.com>
2020-12-19 12:28:12 +0530

    backlog: New structure for storing items
    The previous version would make a separate request for every item in the
    backlog. If the backlog had 10 episodes from a show, it would make 10
    requests to Trakt servers to clear the items. Since this is highly
    inefficient, and might be subject to rate limiting, the new format will
    allow for backlog clearing with a single API request.
    
    Closes #96

--- trakt_scrobbler/backlog_cleaner.py
+++ trakt_scrobbler/backlog_cleaner.py
@@ -1,5 +1,7 @@
 import confuse
 import time
+from collections import defaultdict
+from copy import deepcopy
 from threading import Timer
 from trakt_scrobbler import config, logger
 from trakt_scrobbler.app_dirs import DATA_DIR
@@ -9,15 +11,34 @@ from trakt_scrobbler import trakt_interface as trakt
 
 class BacklogCleaner:
     BACKLOG_PATH = DATA_DIR / "watched_backlog.json"
+    DEFAULT_BACKLOG = {"movies": {}, "shows": {}}
 
     def __init__(self, manual=False):
-        self.backlog = read_json(self.BACKLOG_PATH) or []
         self.clear_interval = config["backlog"]["clear_interval"].get(confuse.Number())
         self.expiry = config["backlog"]["expiry"].get(confuse.Number())
+        self.read_backlog()
         self.timer_enabled = not manual
         if self.timer_enabled:
             self._make_timer()
             self.clear()
+
+    def read_backlog(self):
+        backlog = read_json(self.BACKLOG_PATH)
+        if not backlog:
+            self.backlog = deepcopy(self.DEFAULT_BACKLOG)
+        elif isinstance(backlog, dict):
+            for show in backlog["shows"].values():
+                seasons = defaultdict(dict)
+                for season, episodes in show["seasons"].items():
+                    # JSON keys have to be strings. Convert the nums to int manually.
+                    seasons[int(season)] = {int(num): v for num, v in episodes.items()}
+                show["seasons"] = seasons
+            self.backlog = backlog
+        elif isinstance(backlog, list):  # old backlog style
+            # import the items into new style
+            self.backlog = deepcopy(self.DEFAULT_BACKLOG)
+            for item in backlog:
+                self.add(item)
 
     def save_backlog(self):
         write_json(self.backlog, self.BACKLOG_PATH)
@@ -38,21 +59,33 @@ class BacklogCleaner:
         self.timer.start()
 
     def add(self, data):
-        self.backlog.append(data)
+        media_info = data["media_info"]
+        title = media_info["title"]
+        year = media_info.get("year")
+        key = f"{title}{year if year else ''}"
+
+        if media_info["type"] == "episode":
+            show = self.backlog["shows"].setdefault(key, {
+                "media_info": dict(title=title, year=year, item_type="episode"),
+                "seasons": defaultdict(dict)
+            })
+            season = show["seasons"][media_info["season"]]
+            season[media_info["episode"]] = {
+                "progress": data["progress"],
+                "updated_at": data["updated_at"]
+            }
+        elif media_info["type"] == "movie":
+            self.backlog["movies"][key] = {
+                "media_info": dict(title=title, year=year, item_type="movie"),
+                "progress": data["progress"],
+                "updated_at": data["updated_at"]
+            }
+
         self.save_backlog()
 
     def clear(self):
         self.remove_expired()
-
-        failed = []
-        for item in self.backlog:
-            logger.debug(f'Adding item to history {item}')
-            if trakt.add_to_history(**item):
-                logger.info("Successfully added media to history.")
-            else:
-                failed.append(item)
-
-        self.backlog = failed
+        trakt.bulk_add_to_history(self.backlog)
         self.save_backlog()
 
         if self.timer_enabled:
@@ -60,9 +93,7 @@ class BacklogCleaner:
             self._make_timer()
 
     def purge(self):
-        if not self.backlog:
-            return []
         old_backlog = self.backlog
-        self.backlog = []
+        self.backlog = deepcopy(self.DEFAULT_BACKLOG)
         self.save_backlog()
         return old_backlog
